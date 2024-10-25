df43b488a4220c90a131dbc818e9555687048514
Sasha S.P. <sasa@bubulazy.com>
2024-10-25 22:51:17 +0100

    Update App.js

--- packages/header/src/App.js
+++ packages/header/src/App.js
@@ -6,7 +6,7 @@ document.getElementById("app").innerHTML = `
 <div>Name: header</div>
 <div>Framework: vanilla</div>
 <div>Language: JavaScript</div>
-<div>CSS: Empty CSS</div>
+<div>CSS: Empty CSS </div>
 </div>
 `;
 
